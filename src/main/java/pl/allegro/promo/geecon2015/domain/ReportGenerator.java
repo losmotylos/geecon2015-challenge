3ab9ce20954ab1bc1c63db302e1f9744d90d0b1e
losmotylos <tommy.kaczmarek@gmail.com>
2015-05-14 00:06:21 +0200

    All green!!

--- src/main/java/pl/allegro/promo/geecon2015/domain/ReportGenerator.java
+++ src/main/java/pl/allegro/promo/geecon2015/domain/ReportGenerator.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.allegro.promo.geecon2015.domain.stats.FinancialStatisticsRepository;
 import pl.allegro.promo.geecon2015.domain.transaction.TransactionRepository;
+import pl.allegro.promo.geecon2015.domain.transaction.UserTransaction;
 import pl.allegro.promo.geecon2015.domain.user.UserRepository;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Component
 public class ReportGenerator {
@@ -26,7 +30,32 @@ public class ReportGenerator {
 
     public Report generate(ReportRequest request) {
         Report report = new Report();
+
+        financialStatisticsRepository.listUsersWithMinimalIncome(request.getMinimalIncome(), request.getUsersToCheck())
+                .getUserIds().stream()
+                .map(x -> new ReportedUser(x, getUserName(x), getUserTransactions(x)))
+                .forEach(report::add);
+
         return report;
     }
-    
+
+    private BigDecimal getUserTransactions(UUID userId) {
+        try {
+            return transactionRepository.transactionsOf(userId).getTransactions().stream()
+                    .map(UserTransaction::getAmount)
+                    .reduce(new BigDecimal(0), (x, y) -> x.add(y));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private String getUserName(UUID userId) {
+        try {
+            return userRepository.detailsOf(userId).getName();
+        } catch (Exception e) {
+            return "<failed>";
+        }
+    }
+
 }
+
